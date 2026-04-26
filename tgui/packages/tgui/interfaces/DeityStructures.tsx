0b0bb5bfe5e44ff1a5de885fba5ffaba474cba20
cursedkiller <75551020+cursedkiller@users.noreply.github.com>
2026-04-26 20:02:50 +0100

    Update DeityStructures.tsx

--- tgui/packages/tgui/interfaces/DeityStructures.tsx
+++ tgui/packages/tgui/interfaces/DeityStructures.tsx
@@ -1,59 +1,82 @@
-import { useBackend } from '../backend';
-import { Button, Section, Stack } from '../components';
-import { Window } from '../layouts';
+/datum/tgui/deity_structures
+	tgui_id = "DeityStructures"
 
-type StructureData = {
-  name: string;
-  path: string;
-  icon: string;
-  desc: string;
-  cost: number;
-};
+/datum/tgui/deity_structures/ui_data(mob/user)
+	var/list/data = list()
+	var/mob/living/simple_animal/god/deity = user
 
-type DeityData = {
-  faith: number;
-  max_faith: number;
-  team_colour: string;
-  structures: StructureData[];
-};
+	data["faith"] = deity.faith
+	data["max_faith"] = deity.max_faith
+	data["team_colour"] = deity.team_colour
 
-export const DeityStructures = (props) => {
-  const { act, data } = useBackend<DeityData>();
-  const { faith, max_faith, structures } = data;
+	var/list/structures = list()
+	var/list/available = list(
+		"Power Pylon" = list(/obj/structure/divine/powerpylon, "powerpylon-red", "Generates faith for your deity."),
+		"Translocator" = list(/obj/structure/divine/translocator, "translocator-red", "Allows followers to teleport between translocators."),
+		"Forge" = list(/obj/structure/divine/forge, "forge-red", "Creates divine equipment for followers."),
+		"Sacrifice Altar" = list(/obj/structure/divine/sacrificealtar, "sacrificealtar-red", "Sacrifice beings for gems or faith."),
+		"Conversion Altar" = list(/obj/structure/divine/convertaltar, "convertaltar-red", "Convert crew to your deity."),
+		"Shrine" = list(/obj/structure/divine/shrine, "Shrine-red", "Boosts nearby followers."),
+		"Fountain" = list(/obj/structure/divine/fountain, "fountain-red", "Heals nearby followers."),
+		"Conduit" = list(/obj/structure/divine/conduit, "conduit-red", "Increases faith generation and extends domain."),
+		"Lazarus" = list(/obj/structure/divine/lazarus, "lazarus-r", "Revives a fallen follower once."),
+		"Defense Pylon" = list(/obj/structure/divine/defensepylon, "defensepylon-red", "Attacks non-believers automatically."),
+	)
 
-  return (
-    <Window title="Divine Structures" width={450} height={500}>
-      <Window.Content>
-        <Section
-          title={`Available Faith: ${faith}/${max_faith}`}
-          buttons={
-            <Button
-              icon="times"
-              color="red"
-              onClick={() => act('close')}>
-              Close
-            </Button>
-          }>
-          <Stack vertical>
-            {structures.map((structure) => (
-              <Stack.Item key={structure.name}>
-                <Button
-                  fluid
-                  icon={structure.icon}
-                  disabled={faith < structure.cost}
-                  onClick={() =>
-                    act('build', { path: structure.path })
-                  }
-                  tooltip={structure.desc}>
-                  {structure.name}
-                  {' - '}
-                  {structure.cost} Faith
-                </Button>
-              </Stack.Item>
-            ))}
-          </Stack>
-        </Section>
-      </Window.Content>
-    </Window>
-  );
-};
+	for(var/name in available)
+		var/list/info = available[name]
+		structures += list(list(
+			"name" = name,
+			"path" = "[info[1]]",
+			"icon" = info[2],
+			"desc" = info[3],
+			"cost" = HOG_FAITH_COST_STRUCTURE,
+		))
+
+	data["structures"] = structures
+	return data
+
+/datum/tgui/deity_structures/ui_act(action, params)
+	. = ..()
+	if(action != "build")
+		return
+
+	var/mob/living/simple_animal/god/deity = usr
+	if(!istype(deity))
+		return
+	if(!deity.god_nexus)
+		to_chat(deity, span_warning("You must place your nexus first!"))
+		return
+	if(!deity.can_place_here(get_turf(deity)))
+		to_chat(deity, span_warning("Your domain hasn't reached this area!"))
+		return
+	if(!deity.can_afford(HOG_FAITH_COST_STRUCTURE))
+		return
+
+	var/build_path = text2path(params["path"])
+	if(!build_path)
+		return
+
+	if(build_path == /obj/structure/divine/defensepylon && !deity.free_pylon_used)
+		deity.free_pylon_used = TRUE
+		var/obj/structure/divine/defensepylon/P = new(get_turf(deity))
+		P.assign_deity(deity)
+		to_chat(deity, span_notice("You manifest a defense pylon! Future pylons will require construction."))
+		. = TRUE
+		return
+
+	if(build_path == /obj/structure/divine/convertaltar && !deity.free_conversion_altar_used)
+		deity.free_conversion_altar_used = TRUE
+		var/obj/structure/divine/convertaltar/A = new(get_turf(deity))
+		A.assign_deity(deity)
+		to_chat(deity, span_notice("You manifest a conversion altar! Future altars will require construction."))
+		. = TRUE
+		return
+
+	if(!deity.spend_faith(HOG_FAITH_COST_STRUCTURE))
+		return
+	var/obj/structure/divine/construction_holder/CH = new(get_turf(deity))
+	CH.assign_deity(deity)
+	CH.setup_construction(build_path)
+	CH.visible_message(span_notice("A transparent, unfinished [initial(build_path.name)] appears!"))
+	. = TRUE
