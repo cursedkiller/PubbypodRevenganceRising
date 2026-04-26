7be43cfe4acbf44f6e52a72226acd41ff741f826
cursedkiller <75551020+cursedkiller@users.noreply.github.com>
2026-04-26 20:34:32 +0100

    Update DeityStructures.tsx

--- tgui/packages/tgui/interfaces/DeityStructures.tsx
+++ tgui/packages/tgui/interfaces/DeityStructures.tsx
@@ -1,82 +1,107 @@
-/datum/tgui/deity_structures
-	tgui_id = "DeityStructures"
+import { useBackend } from '../backend';
+import { Button, Section, Stack, Box, Flex, ProgressBar } from '../components';
+import { Window } from '../layouts';
 
-/datum/tgui/deity_structures/ui_data(mob/user)
-	var/list/data = list()
-	var/mob/living/simple_animal/god/deity = user
+type StructureData = {
+  name: string;
+  path: string;
+  icon: string;
+  desc: string;
+  cost: number;
+  materials: string;
+};
 
-	data["faith"] = deity.faith
-	data["max_faith"] = deity.max_faith
-	data["team_colour"] = deity.team_colour
+type DeityData = {
+  faith: number;
+  max_faith: number;
+  team_colour: string;
+  structures: StructureData[];
+};
 
-	var/list/structures = list()
-	var/list/available = list(
-		"Power Pylon" = list(/obj/structure/divine/powerpylon, "powerpylon-red", "Generates faith for your deity."),
-		"Translocator" = list(/obj/structure/divine/translocator, "translocator-red", "Allows followers to teleport between translocators."),
-		"Forge" = list(/obj/structure/divine/forge, "forge-red", "Creates divine equipment for followers."),
-		"Sacrifice Altar" = list(/obj/structure/divine/sacrificealtar, "sacrificealtar-red", "Sacrifice beings for gems or faith."),
-		"Conversion Altar" = list(/obj/structure/divine/convertaltar, "convertaltar-red", "Convert crew to your deity."),
-		"Shrine" = list(/obj/structure/divine/shrine, "Shrine-red", "Boosts nearby followers."),
-		"Fountain" = list(/obj/structure/divine/fountain, "fountain-red", "Heals nearby followers."),
-		"Conduit" = list(/obj/structure/divine/conduit, "conduit-red", "Increases faith generation and extends domain."),
-		"Lazarus" = list(/obj/structure/divine/lazarus, "lazarus-r", "Revives a fallen follower once."),
-		"Defense Pylon" = list(/obj/structure/divine/defensepylon, "defensepylon-red", "Attacks non-believers automatically."),
-	)
+export const DeityStructures = (props) => {
+  const { act, data } = useBackend<DeityData>();
+  const { faith, max_faith, team_colour, structures } = data;
 
-	for(var/name in available)
-		var/list/info = available[name]
-		structures += list(list(
-			"name" = name,
-			"path" = "[info[1]]",
-			"icon" = info[2],
-			"desc" = info[3],
-			"cost" = HOG_FAITH_COST_STRUCTURE,
-		))
+  const faithColor = team_colour === 'red' ? '#ff4444' : '#4444ff';
 
-	data["structures"] = structures
-	return data
-
-/datum/tgui/deity_structures/ui_act(action, params)
-	. = ..()
-	if(action != "build")
-		return
-
-	var/mob/living/simple_animal/god/deity = usr
-	if(!istype(deity))
-		return
-	if(!deity.god_nexus)
-		to_chat(deity, span_warning("You must place your nexus first!"))
-		return
-	if(!deity.can_place_here(get_turf(deity)))
-		to_chat(deity, span_warning("Your domain hasn't reached this area!"))
-		return
-	if(!deity.can_afford(HOG_FAITH_COST_STRUCTURE))
-		return
-
-	var/build_path = text2path(params["path"])
-	if(!build_path)
-		return
-
-	if(build_path == /obj/structure/divine/defensepylon && !deity.free_pylon_used)
-		deity.free_pylon_used = TRUE
-		var/obj/structure/divine/defensepylon/P = new(get_turf(deity))
-		P.assign_deity(deity)
-		to_chat(deity, span_notice("You manifest a defense pylon! Future pylons will require construction."))
-		. = TRUE
-		return
-
-	if(build_path == /obj/structure/divine/convertaltar && !deity.free_conversion_altar_used)
-		deity.free_conversion_altar_used = TRUE
-		var/obj/structure/divine/convertaltar/A = new(get_turf(deity))
-		A.assign_deity(deity)
-		to_chat(deity, span_notice("You manifest a conversion altar! Future altars will require construction."))
-		. = TRUE
-		return
-
-	if(!deity.spend_faith(HOG_FAITH_COST_STRUCTURE))
-		return
-	var/obj/structure/divine/construction_holder/CH = new(get_turf(deity))
-	CH.assign_deity(deity)
-	CH.setup_construction(build_path)
-	CH.visible_message(span_notice("A transparent, unfinished [initial(build_path.name)] appears!"))
-	. = TRUE
+  return (
+    <Window
+      title="Divine Architecture"
+      width={500}
+      height={600}
+      theme={team_colour === 'red' ? 'syndicate' : 'ntos'}>
+      <Window.Content>
+        <Section
+          title={
+            <Flex align="center" justify="space-between" width="100%">
+              <Box fontSize={1.2} bold color={faithColor}>
+                Divine Structures
+              </Box>
+              <Box>
+                <ProgressBar
+                  value={faith}
+                  maxValue={max_faith}
+                  color={faithColor}
+                  width="150px">
+                  {faith}/{max_faith} Faith
+                </ProgressBar>
+              </Box>
+            </Flex>
+          }>
+          <Stack vertical>
+            {structures.map((structure) => (
+              <Stack.Item key={structure.name}>
+                <Button
+                  fluid
+                  disabled={faith < structure.cost}
+                  onClick={() =>
+                    act('build', { path: structure.path })
+                  }
+                  tooltip={structure.desc}>
+                  <Flex align="center" justify="space-between" width="100%">
+                    <Flex.Item>
+                      <Box
+                        as="img"
+                        src={`data:image/png;base64,${structure.icon}`}
+                        width="32px"
+                        height="32px"
+                        mr={1}
+                        style={{
+                          verticalAlign: 'middle',
+                          imageRendering: 'pixelated',
+                        }}
+                      />
+                    </Flex.Item>
+                    <Flex.Item grow={1}>
+                      <Box bold fontSize={1.1}>
+                        {structure.name}
+                      </Box>
+                      <Box fontSize={0.8} opacity={0.7}>
+                        {structure.desc}
+                      </Box>
+                    </Flex.Item>
+                    <Flex.Item textAlign="right">
+                      <Box bold color={faith >= structure.cost ? 'good' : 'bad'}>
+                        {structure.cost} Faith
+                      </Box>
+                      <Box fontSize={0.8} opacity={0.7}>
+                        {structure.materials}
+                      </Box>
+                    </Flex.Item>
+                  </Flex>
+                </Button>
+              </Stack.Item>
+            ))}
+          </Stack>
+        </Section>
+        <Section>
+          <Box textAlign="center" fontSize={0.8} opacity={0.5}>
+            Click a structure to begin construction at your current location.
+            <br />
+            Structures require materials to complete - have your followers add them.
+          </Box>
+        </Section>
+      </Window.Content>
+    </Window>
+  );
+};
